5a618afaf810ef15eeafc4d1358252cca12db88c
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2023-03-28 17:55:08 -0300

    Create quartile.py

---
+++ quartile.py
@@ -0,0 +1,43 @@
+# 'dataset' tem os dados de entrada para este script
+import pandas as pd
+import os
+
+
+def encontra_quartil(lista: list) -> list:
+    amostragem = sorted(lista)
+    indice_principal = int(len(amostragem)/2)
+    # Declara lista Inferior, geral e superior para calcular o quartil
+    consolidado = [amostragem[: indice_principal],
+                   amostragem, amostragem[indice_principal:]]
+    dicionario = {}
+    for idx, lista in enumerate(consolidado):
+        i = int(len(lista)/2)
+        if len(lista) % 2 == 0:
+            q = (lista[i] + lista[i + 1]) / 2
+        else:
+            q = lista[i]
+
+        dicionario[f'{idx + 1}º Quartil'] = q
+
+    return dicionario
+
+
+if __name__ == '__main__':
+    path = r'C:\Users\elias.rodrigues\Unidas\MKT_Geral - Documentos\03.Midia_Geral\01.Midia_em_Geral\00.BI\01.SCRIPTS\coleta_supermetrics_livre'
+    # amostragem = [73, 74, 77, 52, 85, 59, 73, 84, 92]
+    # amostragem = [6, 47, 49, 15, 42, 41, 7, 39, 43, 40, 36]
+
+    df = pd.read_excel(os.path.join(path, 'modelo_vendas.xlsx'))
+    amostragem = df['ROE_%'].to_list()
+    dicionario = encontra_quartil(amostragem)
+    quartil = []
+    for valor in amostragem:
+        if valor < dicionario['1º Quartil']:
+            quartil.append('1º Quartil')
+        elif valor < dicionario['2º Quartil']:
+            quartil.append('2º Quartil')
+        elif valor < dicionario['3º Quartil']:
+            quartil.append('3º Quartil')
+        else:
+            quartil.append('4º Quartil')
+    df['Quartil'] = quartil
